006a978d6737de2e73b2b505eccce9d4bef8480c
Rebekah <rbk1008@Gmail.com>
2022-01-18 23:15:43 -0700

    done

--- app/models/dev.rb
+++ app/models/dev.rb
@@ -2,16 +2,16 @@ class Dev < ActiveRecord::Base
     has_many :freebies
     has_many :companies, through: :freebies
 
-    def recieved_one?(item_name)
-    
+   def received_one?(item_name)
+        self.freebies.any? do |freebie|
+            freebie.item_name == item_name
+        end
     end
+   
 
-    def give_away(dev, freebie)
-        freebie.update(dev: dev)
-    end
-
-
-
+    def give_away(freebie, dev)
+        freebie.update(dev: dev) unless freebie.dev != self
+      end
 
 
 end
--- app/models/freebie.rb
+++ app/models/freebie.rb
@@ -2,10 +2,9 @@ class Freebie < ActiveRecord::Base
     belongs_to :company
     belongs_to :dev
 
-    def print_details
-     "#{dev.name} owns a #{item_name} from #{company.name}"
-    
-    end
+   def print_details
+    "#{dev.name} owns a #{item.name} from #{company.name}`"
+   end
 
 
 
